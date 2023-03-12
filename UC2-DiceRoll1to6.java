402dcad805e241d04dcaec636cf93977e6c608db
“aswinireddy1234” <chegireddyaswinireddy@gmail>
2023-03-12 12:03:15 +0530

    Added: Roll the dices from 1 to 6

---
+++ UC2-DiceRoll1to6.java
@@ -0,0 +1,11 @@
+import java.util.Random;
+public class SnakeAndLadderUc2 {
+    public static void main(String[] args) {
+            int d = 1;
+            int playerpos=0;
+            Random ran = new Random();
+            d = ran.nextInt(6) + 1;
+            System.out.println("dice roll is    " + d);
+
+    }
+}
